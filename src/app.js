36ef9c5e693d9bd6328573181be720e40166da07
Nick Holden <nholden212@gmail.com>
2018-11-08 16:00:58 -0800

    Checksum skeleton complete, need to know what to check against

--- src/app.js
+++ src/app.js
@@ -69,3 +69,31 @@ function parseStr(str){
 
   return result.join('');
 }
+
+function validateAcctNum(acct){
+  var count = 1;
+  var sum = 0;
+  for(let i=8; i>-1; i--){
+    sum += acct[i]*count;
+    count ++;
+  }
+
+  var remainder = sum % 11;
+
+  var checkDigit;
+  var checkAgainst = "?";
+
+  if(remainder === 0){
+    checkDigit = 0;
+  } else if(remainder === 1){
+    return false;
+  } else {
+    checkDigit = 11-remainder;
+  }
+
+  if(checkDigit === checkAgainst){
+    return true;
+  } else {
+    return false;
+  }
+}
